153070e7975166e38b6d28c330ed28eeebdcf80b
mikiprogrammer79 <115552497+mikiprogrammer79@users.noreply.github.com>
2022-11-09 15:33:48 +0000

    Wrap anchor tag

--- index.js
+++ index.js
@@ -16,7 +16,13 @@ inputBtn.addEventListener("click", function() {
 function renderLeads() {
     let listItems = "";
     for (let i = 0; i < myLeads.length; i++) {
-        listItems += "<li>" + myLeads[i] + "</li>";
+        //listItems += "<li><a target= '_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>";
+        listItems += ` <li>
+                            <a target= '_blank' href='${myLeads[i]}'> 
+                            ${myLeads[i]} 
+                            </a>
+                        </li>`;
+
     //const li = document.createElement("li");
     //li.textContent = myLeads[i];
     //ulEl.append(li);
