15a0df14220c0d9353c417fd5b23fc96860ac511
mikiprogrammer79 <115552497+mikiprogrammer79@users.noreply.github.com>
2022-11-09 14:24:03 +0000

    Add HTML CSS and JS files with the basic structure of the Chrome-extension

---
+++ index.js
@@ -0,0 +1,35 @@
+//Create Array to store the leads
+let myLeads = ["pepe", "tato"];
+const inputEl = document.getElementById("input-el"); //This cannot be reassign
+let inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el")
+
+//Activate the save button after click
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value);
+    renderLeads();
+});
+
+//Log out the items of the Array
+function renderLeads() {
+    let listItems = "";
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += "<li>" + myLeads[i] + "</li>";
+    //const li = document.createElement("li");
+    //li.textContent = myLeads[i];
+    //ulEl.append(li);
+};
+
+    ulEl.innerHTML = listItems;
+};
+
+
+
+
+
+
+
+
+
+
+
